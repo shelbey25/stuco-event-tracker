617ec526dc52364c43fb29fccf95ce98cacf6ad4
shelbey25 <shelbe_yousey@asl.org>
2023-04-29 16:37:32 +0100

    Cover

--- src/pages/dressup.tsx
+++ src/pages/dressup.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const DressUp = () => {
   return (
     <div className="flex w-full h-max bg-blue-700 p-4 min-h-screen">
-      
+      BLANK
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,13 @@ const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <div className="flex flex-col p-8 allign-center justify-center w-screen h-screen bg-slate-300">
-      <div className="flex flex-row h-1/2 w-full">
-      <div className="align-middle"><Link href="/dressup"></Link></div>
-      </div>
+    <div className="flex flex-col p-8 items-center justify-center w-screen h-screen bg-slate-300">
+        <div className="flex flex-row items-center h-1/2 w-full p-8 space-x-2 justify-between">
+          <Link className="rounded-lg flex text-3xl font-bold text-center align-middle items-center content-center justify-center w-full h-full bg-blue-400" href="/dressup">Dress-Up Day</Link>
+          <Link className="rounded-lg flex text-3xl font-bold text-center align-middle items-center content-center justify-center w-full h-full bg-blue-400" href="/dressup">Small Booster</Link>
+          <Link className="rounded-lg flex text-3xl font-bold text-center align-middle items-center content-center justify-center w-full h-full bg-blue-400" href="/dressup">Medium Booster</Link>
+          <Link className="rounded-lg flex text-3xl font-bold text-center align-middle items-center content-center justify-center w-full h-full bg-blue-400" href="/dressup">Large Booster</Link>
+        </div>
       <div className="flex justify-self-center h-1/2 w-full"><Leaderboard></Leaderboard></div>
     </div>
   );
