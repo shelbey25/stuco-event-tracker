b80f22f939752e0f68a214edf6993ae3638bdfb8
shelbey25 <shelbe_yousey@asl.org>
2023-04-29 21:08:04 +0100

    Booster Pages Created

--- src/pages/small.tsx
+++ src/pages/small.tsx
@@ -1,12 +1,51 @@
 import { NextPage } from "next";
+import { useState, useEffect, useMemo } from "react";
 import Link from "next/link";
+import { Icon } from "@iconify/react";
+import Events from "../components/Events";
 
 interface Props {}
 
+
 const Small: NextPage = ({}) => {
+    const [events, setEvents] = useState([
+        {
+            name: "Ping Pong",
+            complete: false
+        },
+        {
+            name: "Retro",
+            complete: false
+        },
+        {
+            name: "Sports",
+            complete: true
+        },
+        {
+            name: "Beach",
+            complete: true
+        },
+        {
+            name: "White Lie",
+            complete: false
+        },
+    ]);
+    const sorted = useMemo(
+      () => events.sort((a, b) => (a.complete ? 1 : 0) - (b.complete ? 1 : 0)),
+      [events]
+  );
   return (
-    <div className="flex flex-col p-8 items-center justify-center w-screen h-screen bg-slate-300">
-      small
+    <div className="flex flex-col p-8 items-center justify-start w-screen h-screen bg-slate-300">
+        <div className="flex h-[14] justify-between w-full p-2">
+            <div className="flex justify-center w-1/3"></div>
+            <div className="flex w-1/3 justify-center items-center"><h1 className="heading-primary">Small Boosters</h1></div>
+            <div className="flex justify-end w-1/3">
+                <Link href ="/"><Icon className="w-14 h-14" icon="material-symbols:home" /></Link>
+            </div>
+        </div>
+        <div className="flex h-full w-full p-2">
+            <Events events={sorted}/>
+        </div>
     </div>
   );
 };
