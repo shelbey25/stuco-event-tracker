d30db7bc1c9ea6fa1579d72e6be77c8009a960d0
shelbey25 <shelbe_yousey@asl.org>
2023-04-30 16:29:40 +0100

    Save

--- src/pages/api/router/index.ts
+++ src/pages/api/router/index.ts
@@ -2,12 +2,13 @@ import * as trpc from "@trpc/server";
 import superjson from "superjson";
 import { dressupRouter } from "./dressup";
 import { rankingsRouter } from "./rankings";
+import { z } from 'zod';
+import { createTRPCRouter } from "~/server/api/trpc";
 
-export const appRouter = trpc
-  .router()
-  .transformer(superjson)
-  .merge("dressup.", dressupRouter)
-  .merge("rankings.", rankingsRouter);
+export const appRouter = createTRPCRouter({
+  dressup: dressupRouter,
+  rankings: rankingsRouter
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
