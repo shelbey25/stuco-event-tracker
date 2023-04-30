0c58c085e680755796979d5afe96c24a33438eb9
shelbey25 <shelbe_yousey@asl.org>
2023-04-30 16:29:35 +0100

    Commit before major expirement

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,7 @@
 import * as trpcNext from "@trpc/server/adapters/next";
-import { appRouter } from "~/backend/router/index";
+import { appRouter } from "~/pages/api/router/index";
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => null,
 });
+
