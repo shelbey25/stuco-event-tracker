d5834371a9f69d2fda1bed1c9a0d34342989a85b
shelbey25 <shelbe_yousey@asl.org>
2023-04-30 10:15:43 +0100

    Better UI

--- src/pages/dressup/[name]/index.tsx
+++ src/pages/dressup/[name]/index.tsx
@@ -21,8 +21,34 @@ const Tracker = () => {
                 <Link href ="/"><Icon className="w-14 h-14" icon="material-symbols:home" /></Link>
             </div>
         </div>
-        <div className="flex h-full w-full p-2">
-           
+        <div className="flex h-full items-center w-full p-2 flex-col">
+            <div className="flex gap-x-2 h-14 w-3/4">
+                <div className='w-14'></div>
+                <input className="flex h-8 p-2 rounded-sm text-center w-full" type="text" id="name" name="name"></input>
+                <select className="flex h-8 w-14 rounded-sm" name="grade" id="grade">
+                    <option value="ALL">ALL</option>
+                    <option value="9th">9th</option>
+                    <option value="10th">10th</option>
+                    <option value="11th">11th</option>
+                    <option value="12th">12th</option>
+                </select>
+            </div>
+            <div className="flex h-14 items-end w-1/2 p-2 justify-between">
+                <h1 className="heading-sub w-1/3">Name</h1>
+                <h1 className="heading-sub w-1/3">Seen</h1>
+                <h1 className="heading-sub w-1/3">Participated</h1>
+            </div>
+            <div className="flex h-full items-start w-1/2 p-2 flex">
+                <div className='border-2 border-white flex flex-col items-start w-full'>
+                    {singleEvent.data.map((participant) => (
+                    <div className="flex w-full grid-cols-3">
+                        <div className="border-2 w-full border-r-2 border-white bg-blue-100 p-2"><h1 className='text-center'>{participant.name}</h1></div>
+                        <div className="border-2 w-full border-b-2 justify-center border-r-2 border-white bg-blue-100 p-2"><h1 className='text-center'>{participant.name}</h1></div>
+                        <div className="border-2 w-full border-l-2 justify-center border-white bg-blue-100 p-2"><h1 className='text-center'>{participant.name}</h1></div>
+                    </div>
+                    ))}
+                </div>
+            </div>
         </div>
     </div>
     </div>
