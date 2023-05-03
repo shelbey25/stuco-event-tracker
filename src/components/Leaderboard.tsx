e1b5c45a27ceeeee77c2ca59d80e61eb00e5798e
shelbey25 <shelbe_yousey@asl.org>
2023-05-03 19:31:37 +0100

    removed logs

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -24,27 +24,19 @@ const Leaderboard: React.FC<Props> = ({ }) => {
         points: getTotalPoints(10, 1, data)
       });
     }
-    //Not calling correctly
     const getTotalPoints = (grade: number, id: number, data: Participant[]) => {
         let totalPoints: (number)[] = [];
         let totalNum: (number)[] = [];
         let eventIds: (number)[] = [];
         data.forEach((participant) => {
         if (participant.eventInformationId != null && participant.grade === grade) {
-            console.log(participant)
             if (eventIds.indexOf(participant.eventInformationId) != -1) {
-                console.log("adding")
-                console.log(participant)
                 let validIndex = eventIds.indexOf(participant.eventInformationId)
-                console.log(totalNum[validIndex])
                 totalNum[validIndex] += 1
                 if (participant.dressed) {
                     totalPoints[validIndex] += 1
                 }
-                console.log(totalNum[validIndex])
             } else {
-                console.log("new")
-                console.log(participant)
                 eventIds.push(participant.eventInformationId)
                 let validIndex = eventIds.indexOf(participant.eventInformationId)
                 totalNum.push(1)
@@ -57,17 +49,9 @@ const Leaderboard: React.FC<Props> = ({ }) => {
         }
         )
         let total = 0
-        console.log("Got beef?")
-        console.log(totalNum)
-        console.log(totalPoints)
         eventIds.forEach((eventId, index) => {
-            console.log("Bee")
-            console.log(totalPoints[index])
-            console.log(totalNum[index])
             total = total + Math.round(((100*(totalPoints[index] || 0))/(totalNum[index] || 1)))
-            console.log(total)
         })
-        console.log("Got beef!")
         return total;
     }
 
